202fe8dc214e0e12f262d73e9e0fcbacaa06eda6
Emaduz <135013771+Emaduz@users.noreply.github.com>
2025-08-03 13:51:55 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -86,6 +86,12 @@ module.exports = {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
       },
+      module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        kufi: ['"Noto Kufi Arabic"', 'sans-serif'],
+      },
     },
   },
   plugins: [require('tailwindcss-animate')],
